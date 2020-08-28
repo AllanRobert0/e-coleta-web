4298d841936dfa61536aa4e65f4d293e9dc98391
Allan Silva <allansilva956@gmail.com>
2020-08-27 22:18:11 -0400

    Added react leaflet, an free maps library

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiArrowDownLeft } from "react-icons/fi";
+
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 
@@ -43,6 +46,16 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o Endereço no Mapa</span>
           </legend>
+
+          <Map center={[34.00761, -118.499777]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[34.00761, -118.499777]} />
+          </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
